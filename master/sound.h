ab5c25bd20929062f8c3dacfadc469486dc3c8f9
Pete Madsen <arch.jablonsky@googlemail.com>
2016-06-25 09:09:55 +0200

    Reading from flash correctly.

--- master/sound.h
+++ master/sound.h
@@ -6,9 +6,9 @@
 
 
 // notes in the melody, // note durations: 4 = quarter note, 8 = eighth note, etc.
-const unsigned short melody_enter[] PROGMEM = {
+const int melody_enter[] PROGMEM = {
   NOTE_C4, 4, NOTE_G3, 8, NOTE_G3, 8, NOTE_A3, 4, NOTE_G3, 4, 0, 4, NOTE_B3, 4, NOTE_C4, 4, -1};
-const unsigned short melody_brother_jakob[] PROGMEM = {
+const int melody_brother_jakob[] PROGMEM = {
   NOTE_C4, 4, NOTE_D4, 4, NOTE_E4, 4, NOTE_C4, 4,
   NOTE_C4, 4, NOTE_D4, 4, NOTE_E4, 4, NOTE_C4, 4,
   NOTE_E4, 4, NOTE_F4, 4, NOTE_G4, 2,
@@ -18,7 +18,7 @@ const unsigned short melody_brother_jakob[] PROGMEM = {
   NOTE_C4, 2, NOTE_A3, 2, NOTE_C4, 1,
   NOTE_C4, 2, NOTE_A3, 2, NOTE_C4, 1,
   -1};
-const unsigned short melody_happy_birthday[] PROGMEM = {
+const int melody_happy_birthday[] PROGMEM = {
   NOTE_G4, 4, NOTE_G4, 4, NOTE_A4, 2, NOTE_G4, 2, NOTE_C5, 2, NOTE_B4, 1,
   NOTE_G4, 4, NOTE_G4, 4, NOTE_A4, 2, NOTE_G4, 2, NOTE_D5, 2, NOTE_C5, 1,
   NOTE_G4, 4, NOTE_G4, 4, NOTE_G5, 2, NOTE_E5, 2, NOTE_C5, 2, NOTE_B4, 2, NOTE_A4, 2,
@@ -29,7 +29,7 @@ const unsigned short melody_happy_birthday[] PROGMEM = {
 #define NR_MELODIES 3
 unsigned char sound_next_melody = NR_MELODIES;  // which sound to play
 
-const unsigned short* melodies[] = {
+const int* melodies[] = {
   melody_enter,
   melody_brother_jakob,
   melody_happy_birthday
@@ -71,22 +71,29 @@ bool sound_melody(unsigned char snd)
   }
   
   int thisNote = 0;
-  const unsigned short* notes = melodies[snd];
+  const int* melody = melodies[snd];
 
   Serial.print(F("[snd] playing "));
   Serial.println(snd);
 
   // iterate over the notes of the melody:
-  while (notes[thisNote] != -1) {
+  for (;;) {
+    int note = pgm_read_word(melody + thisNote);
+    if(note == -1) {
+      break;
+    }
+    int next_note = pgm_read_word(melody + thisNote + 1);
+    
     // to calculate the note duration, take one second 
     // divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-    int noteDuration = 1000/notes[thisNote+1];
-    tone(tonePin, notes[thisNote], noteDuration);
+    int noteDuration = 1000/next_note;
+    tone(tonePin, note, noteDuration);
 
     // to distinguish the notes, set a minimum time between them.
     // the note's duration + 30% seems to work well:
     int pauseBetweenNotes = noteDuration * 1.30;
+    Serial.println(pauseBetweenNotes);
     delay(pauseBetweenNotes);
     // stop the tone playing:
     noTone(tonePin);
@@ -94,6 +101,7 @@ bool sound_melody(unsigned char snd)
     // next note
     thisNote += 2;
   }
+  Serial.println(F("[snd] done"));
 
   return true;
 }
